0fe85a2eaacc8aeb38abacb6656284f3ff060b0d
Shynih <jorgepeng2001@gmail.com>
2020-10-08 10:58:07 -0700

    Added test for empty string + 3 additional tests

--- test.cpp
+++ test.cpp
@@ -7,6 +7,27 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+	char * test_val[1]; test_val[0] = "./c-echo";
+	EXPECT_EQ("", echo(1, test_val));
+}
+
+TEST(EchoTest, ThreeWords) {
+	char *test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "three"; test_val[2] = "letter";
+	test_val[3] = "word";
+	EXPECT_EQ("three letter word", echo(4, test_val));
+}
+
+TEST(EchoTest, OneWord) {
+	char *test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "one";
+	EXPECT_EQ("one", echo (2, test_val));
+}
+
+TEST(EchoTest, UnUsedVal) {
+	char *test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "Hi"; test_val[2] = "buddy";
+	EXPECT_EQ("Hi", echo(2, test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
